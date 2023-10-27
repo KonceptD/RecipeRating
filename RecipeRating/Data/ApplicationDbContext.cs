36f6ff582fabf3de91287b45cd2355d22faa0328
KonceptD <kiranjayd@yahoo.com>
2023-10-27 20:04:34 +0200

    Had to delete SQL DB to prevent Identity EF Core clash so I also revised code for appsettings.cs, appplicationdbcontext.cs and program.cs

--- RecipeRating/Data/ApplicationDbContext.cs
+++ RecipeRating/Data/ApplicationDbContext.cs
@@ -5,14 +5,21 @@ using RecipeRating.Models;
 
 namespace RecipeRating.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUserModel, IdentityRole<int>, int>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        : base(options)
         {
         }
 
         public DbSet<RecipeModel> Recipes { get; set; }
         public DbSet<RatingModel> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // ... Your custom model configurations ...
+        }
     }
 }
