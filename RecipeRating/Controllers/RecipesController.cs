28609bb6766ae481d6d7abb9c14b1ec2c88419d7
KonceptD <kiranjayd@yahoo.com>
2023-11-01 17:15:03 +0200

    Added a Recipes page to List recipes, Create one and see details after the user logs in. Controller, 3 Views and 1 Model(Model was done already)

---
+++ RecipeRating/Controllers/RecipesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeRating.Data;
+using RecipeRating.Models;
+
+namespace RecipeRating.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUserModel> _userManager;
+
+        public RecipesController(ApplicationDbContext context, UserManager<AppUserModel> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _context.Recipes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes.FirstOrDefaultAsync(m => m.RecipeID == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RecipeModel recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                recipe.UserID = user.Id;
+                _context.Add(recipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recipe);
+        }
+
+
+    }
+}
